eaba5d4ef2dd3c704e0ddfd52de5782b9807d450
xixiaochao <207875515@qq.com>
2019-07-14 22:24:55 +0800

    node

--- Express/postApplication/test/2_router.js
+++ Express/postApplication/test/2_router.js
@@ -3,6 +3,7 @@ const app = express();
 // next表示执行下一个
 // 路径的分组
 // restul 风格的API接口 GET /user | POST /user | DELETE /user | PUT /user
+// 相同路径的接口只匹配一次就可以了
 app.get('/',function(req,res,next){
     console.log(1);
     next();
